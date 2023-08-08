b8628d65532be073446c0ce5152a5f9931e10242
jshiffert <jshiffert@oki.org>
2023-08-08 17:59:08 -0400

    some controller functions and added model relationships - insomnia request functional

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,3 +2,29 @@ const router = require('express').Router();
 
 const { Post, Comment, User } = require('../models');
 
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: Comment,
+                    include: {
+                        model: User,
+                        attributes: { exclude: ['password']}
+                    }
+                },
+                {
+                    model: User,
+                    attributes: { exclude: ['password']}
+                }
+            ],
+        });
+
+        
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
+// const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
-const dashRoutes = require('./dashRoutes');
+// const dashRoutes = require('./dashRoutes');
 
 router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
-router.use('/dashboard', dashRoutes)
+// router.use('/api', apiRoutes);
+// router.use('/dashboard', dashRoutes)
 
 module.exports = router;
